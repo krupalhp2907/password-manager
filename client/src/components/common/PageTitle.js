b65a2fa9f4e2518c6a80f4a59a11ad8c7a54ff74
krupalhp2907 <krupalhp2907@gmail.com>
2021-12-22 13:38:04 +0530

    Fixed Page Title on mobile device

--- client/src/components/common/PageTitle.js
+++ client/src/components/common/PageTitle.js
@@ -5,20 +5,20 @@ import {GlobeAltIcon, SearchIcon, PencilIcon, TrashIcon} from '@heroicons/react/
 
 function PageTitle({title, noButtons}) {
     return (
-        <div className='border-b pt-6 pb-4 flex justify-between items-center'>
-            <div className=" flex flex-grow items-center space-x-2">
+        <div className='border-b pt-3 pb-4 flex flex-col-reverse md:flex-row'>
+            <div className="flex flex-grow items-center space-x-2">
                 <GlobeAltIcon className='h-6 w-6 text-gray-500 text-center'/>
                 <a href="#" className='text-2xl text-black font-semibold text-center' target='#'>{title}</a>
             </div>
             
             {!noButtons && (
-                <div className=" flex items-center space-x-2">
+                <div className="flex items-center space-x-2 justify-end mb-4 md:mb-auto">
 
-                    <button className='flex items-center space-x-2 w-full font-semibold hover:bg-gray-200 text-sm text-black rounded-md p-2 focus:outline-none focus:bg-gray-200'>
+                    <button className='flex space-x-2 md:mb-auto items-center font-semibold hover:bg-gray-200 text-sm text-black rounded-md p-2 focus:outline-none focus:bg-gray-200'>
                         <PencilIcon className='h-5 w-5'/>
                         <span className=''>Edit</span>
                     </button>
-                    <button className='flex items-center space-x-2 w-full font-semibold hover:bg-gray-200 text-sm tracking-wider text-black rounded-md p-2 focus:outline-none focus:bg-gray-200'>
+                    <button className='flex space-x-2 md:mb-auto items-center font-semibold hover:bg-gray-200 text-sm tracking-wider text-black rounded-md p-2 focus:outline-none focus:bg-gray-200'>
                         <TrashIcon className='h-5 w-5'/>
                         <span className=''>Delete </span>
 
